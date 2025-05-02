78d509412d67c5a0a060595b905bcd6e750fc7c4
Anurag jana <anurag.jana2001@gmail.com>
2025-05-02 12:32:12 +0530

    files added

---
+++ src/main/java/com/example/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.example.ecommerce.repositories;
+
+
+
+import com.example.ecommerce.model.Category;
+import com.example.ecommerce.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
+
+    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
+}
---
+++ src/main/java/com/example/ecommerce/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.service;
+
+
+import com.example.ecommerce.payload.CategoryDTO;
+import com.example.ecommerce.payload.CategoryResponse;
+
+public interface CategoryService {
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+
+    CategoryDTO deleteCategory(Long categoryId);
+
+    CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
+}
